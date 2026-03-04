e65de2b5e3d0cc34b1afbbfb7db08107d7c2176b
Oliver Wright <bwright52@my.bcit.ca>
2026-03-04 12:36:03 -0800

    Implement firebase authentication

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// Global styles (Tailwind + any custom CSS)
+// import '/src/styles/style.css';
+
+// Custom global JS code (shared with all pages) can go here.
+function sayHello() {
+  // TODO: implement your logic here
+}
+
+document.addEventListener('DOMContentLoaded', sayHello);
