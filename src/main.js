32ed796759ff41f66bb06e2fc0407b5f43d3613f
Bill Hodges <bh.hsy@outlook.com>
2026-03-08 22:25:32 -0700

    Display authenticated user's name on homepage

--- src/main.js
+++ src/main.js
@@ -1,68 +1,77 @@
-import { db } from "./firebaseConfig.js";
-import { collection, getDocs, addDoc, serverTimestamp, doc } from "firebase/firestore";
+import { db, auth } from "./firebaseConfig.js";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  serverTimestamp,
+  doc,
+} from "firebase/firestore";
+import { onAuthStateChanged } from "firebase/auth";
 
 function addEventData() {
-  const eventsRef = collection(db, "events")
-  console.log("Adding sample event data.")
+  const eventsRef = collection(db, "events");
+  console.log("Adding sample event data.");
   addDoc(eventsRef, {
-        name: "Placeholder Event",
-        type: "Concert",
-        date: "01-01-01, 3 PM",
-        descShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit quam vitae eros sollicitudin, eget ornare ipsum dignissim. Donec id tortor et leo mollis tincidunt ut at arcu. Nullam consectetur.",
-        previmage: "/images/hike1.jpg",
-        descLong: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at tempus  nisl. Integer suscipit est sed nunc accumsan consequat. Vivamus placerat massa enim, id condimentum velit dictum non. Integer nibh mi, fermentum quis aliquet gravida, feugiat quis dolor. Fusce non lacinia purus, vel  mollis nunc. Curabitur condimentum tincidunt nisl, sit amet vulputate  enim tempus at. Sed feugiat at erat a volutpat. Vivamus bibendum ut nisl dictum pulvinar.",
-        last_updated: serverTimestamp()
-    })
-    addDoc(eventsRef, {
-        name: "Placeholder Event 2",
-        type: "Party",
-        date: "02-02-02, 6 PM",
-        descShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit quam vitae eros sollicitudin, eget ornare ipsum dignissim. Donec id tortor et leo mollis tincidunt ut at arcu. Nullam consectetur.",
-        previmage: "/images/hike2.jpg",
-        descLong: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at tempus  nisl. Integer suscipit est sed nunc accumsan consequat. Vivamus placerat massa enim, id condimentum velit dictum non. Integer nibh mi, fermentum quis aliquet gravida, feugiat quis dolor. Fusce non lacinia purus, vel  mollis nunc. Curabitur condimentum tincidunt nisl, sit amet vulputate  enim tempus at. Sed feugiat at erat a volutpat. Vivamus bibendum ut nisl dictum pulvinar.",
-        last_updated: serverTimestamp()
-    })
-    addDoc(eventsRef, {
-        name: "Placeholder Event 3",
-        type: "Fundraiser",
-        date: "03-03-03, 9 PM",
-        descShort: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit quam vitae eros sollicitudin, eget ornare ipsum dignissim. Donec id tortor et leo mollis tincidunt ut at arcu. Nullam consectetur.",
-        previmage: "/images/hike3.jpg",
-        descLong: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at tempus  nisl. Integer suscipit est sed nunc accumsan consequat. Vivamus placerat massa enim, id condimentum velit dictum non. Integer nibh mi, fermentum quis aliquet gravida, feugiat quis dolor. Fusce non lacinia purus, vel  mollis nunc. Curabitur condimentum tincidunt nisl, sit amet vulputate  enim tempus at. Sed feugiat at erat a volutpat. Vivamus bibendum ut nisl dictum pulvinar.",
-        last_updated: serverTimestamp()
-    });
+    name: "Placeholder Event",
+    type: "Concert",
+    date: "01-01-01, 3 PM",
+    descShort:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit quam vitae eros sollicitudin, eget ornare ipsum dignissim. Donec id tortor et leo mollis tincidunt ut at arcu. Nullam consectetur.",
+    previmage: "/images/hike1.jpg",
+    descLong:
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at tempus  nisl. Integer suscipit est sed nunc accumsan consequat. Vivamus placerat massa enim, id condimentum velit dictum non. Integer nibh mi, fermentum quis aliquet gravida, feugiat quis dolor. Fusce non lacinia purus, vel  mollis nunc. Curabitur condimentum tincidunt nisl, sit amet vulputate  enim tempus at. Sed feugiat at erat a volutpat. Vivamus bibendum ut nisl dictum pulvinar.",
+    last_updated: serverTimestamp(),
+  });
+  addDoc(eventsRef, {
+    name: "Placeholder Event 2",
+    type: "Party",
+    date: "02-02-02, 6 PM",
+    descShort:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit quam vitae eros sollicitudin, eget ornare ipsum dignissim. Donec id tortor et leo mollis tincidunt ut at arcu. Nullam consectetur.",
+    previmage: "/images/hike2.jpg",
+    descLong:
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at tempus  nisl. Integer suscipit est sed nunc accumsan consequat. Vivamus placerat massa enim, id condimentum velit dictum non. Integer nibh mi, fermentum quis aliquet gravida, feugiat quis dolor. Fusce non lacinia purus, vel  mollis nunc. Curabitur condimentum tincidunt nisl, sit amet vulputate  enim tempus at. Sed feugiat at erat a volutpat. Vivamus bibendum ut nisl dictum pulvinar.",
+    last_updated: serverTimestamp(),
+  });
+  addDoc(eventsRef, {
+    name: "Placeholder Event 3",
+    type: "Fundraiser",
+    date: "03-03-03, 9 PM",
+    descShort:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris suscipit quam vitae eros sollicitudin, eget ornare ipsum dignissim. Donec id tortor et leo mollis tincidunt ut at arcu. Nullam consectetur.",
+    previmage: "/images/hike3.jpg",
+    descLong:
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at tempus  nisl. Integer suscipit est sed nunc accumsan consequat. Vivamus placerat massa enim, id condimentum velit dictum non. Integer nibh mi, fermentum quis aliquet gravida, feugiat quis dolor. Fusce non lacinia purus, vel  mollis nunc. Curabitur condimentum tincidunt nisl, sit amet vulputate  enim tempus at. Sed feugiat at erat a volutpat. Vivamus bibendum ut nisl dictum pulvinar.",
+    last_updated: serverTimestamp(),
+  });
 }
 
 async function seedEvents() {
+  const eventsRef = collection(db, "events");
 
-  const eventsRef = collection(db, "events")
-
-  const querySnapshot = await getDocs(eventsRef)
+  const querySnapshot = await getDocs(eventsRef);
 
   if (querySnapshot.empty) {
-    console.log("Events collection is empty. Seeding data.")
+    console.log("Events collection is empty. Seeding data.");
 
-    addEventData()
-  }
-
-  else {
-    console.log("Event collection already contains data. Skipping seed.")
+    addEventData();
+  } else {
+    console.log("Event collection already contains data. Skipping seed.");
   }
 }
 
-seedEvents()
+seedEvents();
 
-
-async function displayCardsDynamically(){
-  const eventsCollectionRef = collection(db, "events")
+async function displayCardsDynamically() {
+  const eventsCollectionRef = collection(db, "events");
 
   try {
-    const querySnapshot = await getDocs(eventsCollectionRef)
-    querySnapshot.forEach((doc) =>{
-    let newEvent = doc.data()
-    let result_html = ""
-    result_html += ``
-    result_html += `
+    const querySnapshot = await getDocs(eventsCollectionRef);
+    querySnapshot.forEach((doc) => {
+      let newEvent = doc.data();
+      let result_html = "";
+      result_html += ``;
+      result_html += `
       <div class="p-4 shadow-2xl border-slate-200 border-2 rounded-2xl lg:w-80">
           <img class="rounded-3xl my-1" src=${newEvent.previmage} alt="eventimg"/>
           <div class="flex gap-4 mb-2">
@@ -86,18 +95,46 @@ async function displayCardsDynamically(){
           
         </div>
       
-    `
+    `;
 
-    let result_div = ""
-    result_div = document.getElementById("eventHolder")
-    let x = document.createElement(`div`)
-    x.innerHTML = result_html
-    result_div.appendChild(x)
-})
-}
-catch (error) {
-  console.error("Error getting documents:", error)
-}
+      let result_div = "";
+      result_div = document.getElementById("eventHolder");
+      let x = document.createElement(`div`);
+      x.innerHTML = result_html;
+      result_div.appendChild(x);
+    });
+  } catch (error) {
+    console.error("Error getting documents:", error);
+  }
 }
 
-displayCardsDynamically()
+displayCardsDynamically();
+
+// ADDed NAME UPDATE TO HOME PAGE CARD
+function showName() {
+  const nameElement = document.getElementById("name-goes-here"); // the <h1> element to display "Hello, {name}"
+
+  // Wait for Firebase to determine the current authentication state.
+  // onAuthReady() runs the callback once Firebase finishes checking the signed-in user.
+  // The user's name is extracted from the Firebase Authentication object
+  // You can "go to console" to check out current users.
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log("LOGGED IN as:", user.uid, user.email);
+      // e.g., show authed UI
+    } else {
+      console.log("NOT logged in");
+      // e.g., show guest UI
+    }
+
+    const name = user.displayName || user.email;
+
+    if (nameElement) {
+      nameElement.textContent = name;
+    }
+  });
+}
+
+showName();
+// ENDS HERE
