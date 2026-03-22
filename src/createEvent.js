13467b0ebfd3ed7f42858fe920c8a735c967ddb1
Oliver Wright <bwright52@my.bcit.ca>
2026-03-22 13:27:48 -0700

    Add functionality for uploading basic event data but not images

--- src/createEvent.js
+++ src/createEvent.js
@@ -5,22 +5,29 @@ import {
   addDoc,
   serverTimestamp,
   doc,
-  uploadBytes
 } from "firebase/firestore";
+import {uploadBytes} from "firebase/storage"
 
 function uploadEvent(){
+    // to do: grab new event id, create new collection in event called images, use uploadbytes to attempt to place images there
     const eventsRef = collection(db, "events");
     const eventTitle = document.getElementById("title").value
     const eventTime = document.getElementById("time").value
     const eventType = document.getElementById("type").value
     const eventDesc = document.getElementById("description").value
     const eventImg = document.getElementById("prevImg").value
+    // this didn't work.
+    // uploadBytes(eventsRef, eventImg).then((snapshot) => {
+    //     console.log("hopefully this works")
+    // })
     addDoc(eventsRef, {
         name: eventTitle,
         date: eventTime,
         descLong: eventDesc,
         descShort: eventDesc,
         type: eventType,
+        // this didn't work either.
+        // previmage:eventImg
         last_updated: serverTimestamp(),
     })
 }
