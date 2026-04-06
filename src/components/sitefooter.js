38b45decbdb5c2b094c5ee5d7528e2339f3c0d67
Bill Hodges <bh.hsy@outlook.com>
2026-04-05 20:25:57 -0700

    Update and simplify footer

--- src/components/sitefooter.js
+++ src/components/sitefooter.js
@@ -7,39 +7,9 @@ class SiteFooter extends HTMLElement {
   renderFooter() {
     this.innerHTML = `
       <footer class="bg-gray-900 text-gray-200 py-10">
-        <div class="max-w-full justify-center px-4 flex md:gap-60 gap-14">
-          <div>
-            <h3 class="text-sm font-semibold text-gray-400 uppercase mb-4">
-              Services
-            </h3>
-            <ul class="space-y-2">
-              <li><a href="#" class="hover:text-white">Branding</a></li>
-              <li><a href="#" class="hover:text-white">Design</a></li>
-              <li><a href="#" class="hover:text-white">Marketing</a></li>
-              <li><a href="#" class="hover:text-white">Advertisement</a></li>
-            </ul>
-          </div>
-          <div>
-            <h3 class="text-sm font-semibold text-gray-400 uppercase mb-4">
-              Company
-            </h3>
-            <ul class="space-y-2">
-              <li><a href="#" class="hover:text-white">About us</a></li>
-              <li><a href="#" class="hover:text-white">Contact</a></li>
-              <li><a href="#" class="hover:text-white">Jobs</a></li>
-              <li><a href="#" class="hover:text-white">Press kit</a></li>
-            </ul>
-          </div>
-          <div>
-            <h3 class="text-sm font-semibold text-gray-400 uppercase mb-4">
-              Legal
-            </h3>
-            <ul class="space-y-2">
-              <li><a href="#" class="hover:text-white">Terms of use</a></li>
-              <li><a href="#" class="hover:text-white">Privacy policy</a></li>
-              <li><a href="#" class="hover:text-white">Cookie policy</a></li>
-            </ul>
-          </div>
+        <div class="text-center space-y-1">
+        <p class="text-md font-semibold">DTC-06</p>
+        <p class="text-base">© 2026 Guidebook Vancouver. All rights reserved.</p>
         </div>
       </footer>
     `;
