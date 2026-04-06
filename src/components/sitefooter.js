a9b6c87e0c5e17cc1e78db27ffaf9cb91fc5450e
Bill Hodges <bh.hsy@outlook.com>
2026-04-06 13:40:19 -0700

    Adjust footer to prevent overlap with navbar in mobile

--- src/components/sitefooter.js
+++ src/components/sitefooter.js
@@ -6,10 +6,10 @@ class SiteFooter extends HTMLElement {
 
   renderFooter() {
     this.innerHTML = `
-      <footer class="bg-gray-900 text-gray-200 py-10">
+      <footer class="bg-gray-900 text-gray-200 py-10 pb-24 md:pb-10">
         <div class="text-center space-y-1">
         <p class="text-md font-semibold">DTC-06</p>
-        <p class="text-base">© 2026 Guidebook Vancouver. All rights reserved.</p>
+        <p class="text-base opacity-50">© 2026 Guidebook Vancouver. All rights reserved.</p>
         </div>
       </footer>
     `;
@@ -17,3 +17,6 @@ class SiteFooter extends HTMLElement {
 }
 
 customElements.define("site-footer", SiteFooter);
+
+
+
